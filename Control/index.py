68b4524826844afea5fe9849656d497c76d83094
Lewis Porter <lewis.porter@gmail.com>
2013-11-24 16:02:23 +0100

    Intial Upload

---
+++ Control/index.py
@@ -0,0 +1,70 @@
+﻿#!/usr/bin/python
+
+#import modules
+import RPi.GPIO as GPIO
+import cgi, cgitb 
+import virtuallcd
+
+#enable debugging output
+cgitb.enable()
+
+#define functions for repetitive html stuff
+def printHeader():
+	print "<!DOCTYPE html><!-- <!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\"> --><!-- <!DOCTYPE html> -->"
+	print "<html>"
+	print "<head>"
+	print "<meta http-equiv=\"Content-type\" content=\"text/html;charset=UTF-8\">"
+	print"<link rel=\"stylesheet\" type=\"text/css\" href=\"styles/stylesheet.css\">"
+	print "<title>CoffeeControl v0.1</title>"
+	print "</head>"
+	print "<body>"
+	print "<div id=\"container\">"
+	print "<div id=\"header\"><h1>Coffee Control Panel</h1></div>"
+	print "<div id=\"menu\"><b>Menu</b><br>Coffee<br></div>"
+	
+def printFooter():
+	print "</div>"
+	print "</body>"
+	print "</html>"
+	
+def  printControls():
+	print "<div id=\"content\">"
+	print "<img src=\"output.png\" style=\"height:50px;width:300px;image-rendering:-webkit-optimize-contrast;\">"
+	print "<form action=\"index.py\" method=\"post\">"
+	print "<select name=\"coffeeselection\">"
+	print "<option value=\"cappuccino\">Cappuccino</option>"
+	print "<option value=\"espresso\">Espresso</option>"
+	print "</select>"
+	print "<input type=\"submit\" value=\"Brew\"/>"
+	print "</form>"
+
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
+
+#set up GPIO Mode
+GPIO.setmode(GPIO.BOARD)
+
+#set up GPIO Channel
+GPIO.setup(23, GPIO.OUT)
+
+printHeader()
+
+if form.getvalue('coffeeselection') == "cappuccino":
+	printControls()
+	print "Making Cappuccino</div>"
+	GPIO.output(23, GPIO.HIGH)
+	GPIO.output(23, GPIO.LOW)
+	
+elif form.getvalue('coffeeselection') == "espresso":
+	printControls()
+	print "Making Espresso</div>"
+	GPIO.output(23, GPIO.HIGH)
+	GPIO.output(23, GPIO.LOW)
+	
+#if the webpage has been opened for the first time, do this
+else:
+	printControls()
+	
+
+printFooter()
+GPIO.cleanup()
---
+++ LCD/virtuallcd.py
@@ -0,0 +1,62 @@
+﻿#!/usr/bin/python
+
+#import modules
+import cgi, cgitb 
+import PIL
+import Image
+
+#enable debugging output
+cgitb.enable()
+
+# Create instance of FieldStorage 
+textInput = cgi.FieldStorage() 
+
+#initialise LCD
+lcdContents = [[0],[0]]
+lcdContents[0] = textInput.getvalue('rowOne')
+lcdContents[1] = textInput.getvalue('rowTwo')
+background = Image.open("images/lcd/lcdtemplate.png")
+
+#LCD functions
+def drawChar(x,y):
+	if lcdContents[x][y] == " ":
+		pass
+	elif lcdContents[x][y] == ".":
+		charImage = Image.open("images/lcd/characters/dot.png")
+		if x == 0:
+			background.paste(charImage, (25*y+4-y, 4), charImage)
+		elif x == 1:
+			background.paste(charImage, (25*y+4-y, 40), charImage)
+		else:
+			raise Exception("drawChar function trying to draw on nonexistent row!")
+			
+		
+	else:
+		charImage = Image.open("images/lcd/characters/" + lcdContents[x][y] + ".png")
+		if x == 0:
+			background.paste(charImage, (25*y+4-y, 4), charImage)
+		elif x == 1:
+			background.paste(charImage, (25*y+4-y, 40), charImage)
+		else:
+			raise Exception("drawChar function trying to draw on nonexistent row!")
+			
+def drawLine(line):
+	if textInput.getvalue('rowOne') == None and line == 0:
+		pass
+	elif textInput.getvalue('rowTwo') == None and line == 1:
+		pass
+	else:
+		charsRemaining = len(lcdContents[line]) - 1
+		while charsRemaining >= 0:
+			drawChar(line,charsRemaining)
+			charsRemaining = charsRemaining - 1
+		
+def drawDisplay():
+	drawLine(0)
+	drawLine(1)
+
+
+#program flow begins here		
+
+drawDisplay()
+background.save("output.png")
